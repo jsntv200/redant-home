2468ef816068bfdbd871e74887411f7754a219db
Jason Taylor <jason@redant.com.au>
2023-02-23 10:26:50 +1100

    Update ENV to PUBLIC namespace

--- .tina/config.js
+++ .tina/config.js
@@ -5,8 +5,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "master"
 
 export default defineConfig({
   branch,
-  clientId: process.env.TINA__CLIENT_ID, // Get this from tina.io
-  token: process.env.TINA__TOKEN, // Get this from tina.io
+  clientId: process.env.TINA_PUBLIC_CLIENT_ID, // Get this from tina.io
+  token: process.env.TINA_PUBLIC_TOKEN, // Get this from tina.io
   build: {
     outputFolder: "admin",
     publicFolder: "./",
