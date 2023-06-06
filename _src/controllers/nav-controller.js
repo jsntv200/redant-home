4faea5544715250f79c081529f6a81bda8426f9a
Paulo Pao <paulo@redant.com.au>
2023-06-06 18:06:06 +1000

    fix(restyle): Rollback subpages & put new layouts in tailwind folders

--- _src/controllers/nav-controller.js
+++ _src/controllers/nav-controller.js
@@ -5,16 +5,22 @@ export class NavController extends Controller {
 
   static values = {
     activeClass: { type: String, default: "active" },
+    scrollAnimation: { type: String, default: "false" },
   };
 
   connect() {
     if (!this.items) return;
     this.toggleActive();
-    window.addEventListener('scroll', this.handleScroll.bind(this));
+
+    if (this.scrollAnimationValue === "true") {
+      window.addEventListener('scroll', this.handleScroll.bind(this));
+    }
   }
 
   disconnect() {
-    window.removeEventListener('scroll', this.handleScroll.bind(this));
+    if (this.scrollAnimationValue === "true") {
+      window.removeEventListener('scroll', this.handleScroll.bind(this));
+    }
   }
 
   handleScroll() {
@@ -23,12 +29,6 @@ export class NavController extends Controller {
     } else {
       this.element.classList.remove('bg-black');
     }
-  }
-
-  toggleActive() {
-    this.items
-      .filter(this.isActive.bind(this))
-      .forEach(this.setActive.bind(this));
   }
 
   isActive(link) {
@@ -49,6 +49,12 @@ export class NavController extends Controller {
     if (height) {
       this.element.style.height = `${height}px`;
     }
+  }
+
+  toggleActive() {
+    this.items
+      .filter(this.isActive.bind(this))
+      .forEach(this.setActive.bind(this));
   }
 
   get activeClass() {
